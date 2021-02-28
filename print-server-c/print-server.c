c77e1cf1045d0f0dbce36caa56c47c5db7b18892
BubuHub <66834304+BubuHub@users.noreply.github.com>
2021-02-28 18:01:39 +0100

    License fixups

--- print-server-c/print-server.c
+++ print-server-c/print-server.c
@@ -2,7 +2,10 @@
  * Server to controll 3D printer.
  *
  * Author: Rafal Vonau <rafal.vonau@gmail.com>
- *
+ #
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
  */
 #include "sys_net.h"
 #include <dlfcn.h>
